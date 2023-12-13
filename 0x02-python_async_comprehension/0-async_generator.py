fe50329256fe28a38e8aa23d6a736da75fae9617
CARTOON <brianngugi224@gmail.com>
2023-12-13 11:55:32 +0300

    Fixig an issue with import

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-''' 
-    Task Zero
 '''
-from typing import Generator
-import random
+    Task 0's module.
+'''
 import asyncio
+import random
+from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
-    '''
-        Async Generator
-    '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random * 10
+        yield random.random() * 10
