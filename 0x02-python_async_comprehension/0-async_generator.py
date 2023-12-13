56284c1ec1b2f5016eb9eedbf08ce8b1c451ef6a
CARTOON <brianngugi224@gmail.com>
2023-12-13 12:03:06 +0300

    Task execution

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
