edc65da4dcce6174aaa4fb64d598888c021bde46
Cartoon <brianngugi224@gmail.com>
2025-05-17 11:11:31 +0300

    add a return

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -28,4 +28,5 @@ def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
         for user in batch: 
             if user['age'] > 25:
-                print(user)  
+                print(user) 
+    return
