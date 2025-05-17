e973b498726c792944fd80878ad60c5f3ccee9c2
Cartoon <brianngugi224@gmail.com>
2025-05-17 10:37:06 +0300

    add return

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -11,7 +11,6 @@ def stream_users_in_batches(batch_size):
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
     )
-
     cursor = conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
 
@@ -19,12 +18,14 @@ def stream_users_in_batches(batch_size):
         batch = cursor.fetchmany(batch_size)
         if not batch:
             break
-        yield batch
+        yield batch  
+
     cursor.close()
     conn.close()
 
+
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
-        for user in batch:
+        for user in batch: 
             if user['age'] > 25:
-                print(user)
+                print(user)  
