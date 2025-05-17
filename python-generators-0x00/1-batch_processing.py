0ed84805f3d235fd7bdccdd298e4e065cec555ad
Cartoon <brianngugi224@gmail.com>
2025-05-17 10:34:22 +0300

    processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def stream_users_in_batches(batch_size):
+    conn = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+    cursor.close()
+    conn.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
