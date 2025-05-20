7f9254011940e9fbfd4b02a73632fd2afb1e8481
Cartoon <brianngugi224@gmail.com>
2025-05-20 18:14:44 +0300

    continous fetching

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiosqlite
+
+DB_NAME = "users.db"
+
+async def async_fetch_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            print("[ALL USERS]:", users)
+            return users
+        
+async def async_fetch_older_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            older_users = await cursor.fetchall()
+            print("[USERS OVER 40]:", older_users)
+            return older_users
+        
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+asyncio.run(fetch_concurrently())
