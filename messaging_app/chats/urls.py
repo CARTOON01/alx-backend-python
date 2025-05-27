3a13a9fb02e4be62d84b9f130deb3c5205764cfa
Cartoon <brianngugi224@gmail.com>
2025-05-27 12:53:22 +0300

    fix(urls): correct import statement for admin in urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,7 @@
 from rest_franework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
+from django.contrib import admin
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
@@ -8,4 +9,6 @@ router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls'))
 ]
